ed7bb76016a514bf40b41323a0fa9c38c9a9b670
SolyaraDaDaYa <enderderder@gmail.com>
2025-05-18 23:12:17 +0400

    Add service methods

--- internal/service/post/service.go
+++ internal/service/post/service.go
@@ -117,14 +117,14 @@ func (s *PostService) CreatePost(ctx context.Context, post *model.CreatePostDTO)
 			createdTag, err := tagRepo.Create(ctx, name)
 			if err != nil {
 				if errors.Is(err, custom_errors.ErrTagAlreadyExists) {
-					s.log.Debug("Тег уже существует", slog.String("error", err.Error()))
+					s.log.Debug("Tag already exists", slog.String("error", err.Error()))
 					return nil, custom_errors.ErrTagAlreadyExists
 				}
 				if errors.Is(err, custom_errors.ErrTagCreateFailed) {
-					s.log.Error("Ошибка создания тега", slog.String("error", err.Error()))
+					s.log.Error("Failed to create tag", slog.String("error", err.Error()))
 					return nil, custom_errors.ErrTagCreateFailed
 				}
-				s.log.Error("Неизвестная ошибка при создании тега", slog.String("error", err.Error()))
+				s.log.Error("Unknown error while creating tag", slog.String("error", err.Error()))
 				return nil, err
 			}
 			createdTags = append(createdTags, createdTag)
@@ -133,22 +133,22 @@ func (s *PostService) CreatePost(ctx context.Context, post *model.CreatePostDTO)
 		err = tagRepo.TagPost(ctx, createdPost.ID, post.Tags)
 		if err != nil {
 			if errors.Is(err, custom_errors.ErrPostNotFound) {
-				s.log.Debug("Пост не найден при добавлении тегов", slog.String("error", err.Error()))
+				s.log.Debug("Post not found when adding tags", slog.String("error", err.Error()))
 				return nil, custom_errors.ErrPostNotFound
 			}
 			if errors.Is(err, custom_errors.ErrTagNotFound) {
-				s.log.Debug("Тег не найден при добавлении к посту", slog.String("error", err.Error()))
+				s.log.Debug("Tag not found when adding to post", slog.String("error", err.Error()))
 				return nil, custom_errors.ErrTagNotFound
 			}
 			if errors.Is(err, custom_errors.ErrTagVerifyPostFailed) {
-				s.log.Error("Ошибка верификации поста при добавлении тегов", slog.String("error", err.Error()))
+				s.log.Error("Tag verification failed when adding tags to post", slog.String("error", err.Error()))
 				return nil, custom_errors.ErrTagVerifyPostFailed
 			}
 			if errors.Is(err, custom_errors.ErrTagPost) {
-				s.log.Error("Ошибка добавления тегов к посту", slog.String("error", err.Error()))
+				s.log.Error("Failed to add tags to post", slog.String("error", err.Error()))
 				return nil, custom_errors.ErrTagPost
 			}
-			s.log.Error("Неизвестная ошибка при добавлении тегов", slog.String("error", err.Error()))
+			s.log.Error("Unknown error while adding tags to post", slog.String("error", err.Error()))
 			return nil, err
 		}
 	}
@@ -297,9 +297,192 @@ func (s *PostService) ListPosts(ctx context.Context, filters *model.PostFilters)
 }
 
 func (s *PostService) UpdatePost(ctx context.Context, id int64, post *model.UpdatePostDTO) error {
-	panic("gbergreg")
+	tx, err := s.uow.Begin(ctx)
+	if err != nil {
+		s.log.Error("Failed to start transaction", slog.String("error", err.Error()))
+		return custom_errors.ErrDatabaseQuery
+	}
+	defer func() {
+		if err != nil {
+			_ = tx.Rollback(ctx)
+		}
+	}()
+
+	postRepo := tx.PostRepository()
+	mediaRepo := tx.MediaRepository()
+	tagRepo := tx.TagRepository()
+
+	_, err = postRepo.Update(ctx, id, post)
+	if err != nil {
+		if errors.Is(err, custom_errors.ErrPostNotFound) {
+			s.log.Debug("Post not found for update", slog.Int64("id", id))
+			return custom_errors.ErrPostNotFound
+		}
+		s.log.Error("Failed to update post", slog.String("error", err.Error()), slog.Int64("id", id))
+		return custom_errors.ErrDatabaseQuery
+	}
+
+	if post.MediaItems != nil {
+		media, err := mediaRepo.GetByPost(ctx, id)
+		if err != nil {
+			if errors.Is(err, custom_errors.ErrMediaNotFound) {
+				s.log.Debug("Media not found for update", slog.Int64("id", id))
+				return custom_errors.ErrMediaNotFound
+			}
+			s.log.Error("Failed to get post media", slog.String("error", err.Error()), slog.Int64("id", id))
+			return custom_errors.ErrDatabaseQuery
+		}
+		mediaIds := make([]int64, 0, len(media))
+		for _, mediaItem := range media {
+			mediaIds = append(mediaIds, mediaItem.ID)
+		}
+		err = mediaRepo.Detach(ctx, mediaIds)
+		if err != nil {
+			s.log.Error("Failed to clear media for post", slog.String("error", err.Error()), slog.Int64("id", id))
+			return custom_errors.ErrMediaAttachFailed
+		}
+		if len(post.MediaItems) > 0 {
+			media := make([]*model.PostMedia, 0, len(post.MediaItems))
+			for _, m := range post.MediaItems {
+				media = append(media, &model.PostMedia{
+					PostID:   id,
+					URL:      m.URL,
+					Type:     m.Type,
+					Position: m.Position,
+				})
+			}
+			err = mediaRepo.Attach(ctx, id, media)
+			if err != nil {
+				s.log.Error("Failed to attach media to post", slog.String("error", err.Error()), slog.Int64("id", id))
+				return custom_errors.ErrMediaAttachFailed
+			}
+		}
+	}
+
+	if post.Tags != nil && len(post.Tags) > 0 {
+		for _, name := range post.Tags {
+			_, err := tagRepo.Create(ctx, name)
+			if err != nil && !errors.Is(err, custom_errors.ErrTagAlreadyExists) {
+				if errors.Is(err, custom_errors.ErrTagCreateFailed) {
+					s.log.Error("Failed to create tag", slog.String("error", err.Error()))
+					return custom_errors.ErrTagCreateFailed
+				}
+				s.log.Error("Unknown error creating tag", slog.String("error", err.Error()))
+				return err
+			}
+		}
+		err = tagRepo.ReplacePostTags(ctx, id, post.Tags)
+		if err != nil {
+			if errors.Is(err, custom_errors.ErrPostNotFound) {
+				s.log.Debug("Post not found when tagging", slog.String("error", err.Error()))
+				return custom_errors.ErrPostNotFound
+			}
+			if errors.Is(err, custom_errors.ErrTagNotFound) {
+				s.log.Debug("Tag not found when tagging post", slog.String("error", err.Error()))
+				return custom_errors.ErrTagNotFound
+			}
+			if errors.Is(err, custom_errors.ErrTagVerifyPostFailed) {
+				s.log.Error("Tag verify post failed", slog.String("error", err.Error()))
+				return custom_errors.ErrTagVerifyPostFailed
+			}
+			if errors.Is(err, custom_errors.ErrTagPost) {
+				s.log.Error("Failed to tag post", slog.String("error", err.Error()))
+				return custom_errors.ErrTagPost
+			}
+			s.log.Error("Unknown error tagging post", slog.String("error", err.Error()))
+			return err
+		}
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		s.log.Error("Failed to commit transaction", slog.String("error", err.Error()))
+		return custom_errors.ErrDatabaseQuery
+	}
+
+	return nil
 }
 
 func (s *PostService) DeletePost(ctx context.Context, id int64) error {
-	panic("implement")
+	tx, err := s.uow.Begin(ctx)
+	if err != nil {
+		s.log.Error("Failed to start transaction", slog.String("error", err.Error()))
+		return custom_errors.ErrDatabaseQuery
+	}
+	defer func() {
+		if err != nil {
+			_ = tx.Rollback(ctx)
+		}
+	}()
+
+	postRepo := tx.PostRepository()
+	mediaRepo := tx.MediaRepository()
+	tagRepo := tx.TagRepository()
+
+	media, err := mediaRepo.GetByPost(ctx, id)
+	if err != nil {
+		if errors.Is(err, custom_errors.ErrMediaNotFound) {
+			s.log.Debug("Media not found for post during delete", slog.Int64("id", id))
+			media = nil
+		} else {
+			s.log.Error("Failed to get media for post during delete", slog.String("error", err.Error()), slog.Int64("id", id))
+			return custom_errors.ErrMediaQueryFailed
+		}
+	}
+	mediaIds := make([]int64, 0, len(media))
+	for _, mediaItem := range media {
+		mediaIds = append(mediaIds, mediaItem.ID)
+	}
+	if len(mediaIds) > 0 {
+		err = mediaRepo.Detach(ctx, mediaIds)
+		if err != nil {
+			if errors.Is(err, custom_errors.ErrMediaNotFound) {
+				s.log.Debug("Media not found for post during detach", slog.Int64("id", id))
+			} else {
+				s.log.Error("Failed to detach media for post", slog.String("error", err.Error()), slog.Int64("id", id))
+				return custom_errors.ErrMediaDetachFailed
+			}
+		}
+	}
+
+	tags, err := tagRepo.FindByPost(ctx, id)
+	if err != nil {
+		if errors.Is(err, custom_errors.ErrTagsNotFound) {
+			s.log.Debug("Tags not found for post during delete", slog.Int64("id", id))
+			tags = nil
+		} else {
+			s.log.Error("Failed to get tags for post during delete", slog.String("error", err.Error()), slog.Int64("id", id))
+			return custom_errors.ErrTagQueryFailed
+		}
+	}
+	tagNames := make([]string, 0, len(tags))
+	for _, tag := range tags {
+		tagNames = append(tagNames, tag.Name)
+	}
+	if len(tagNames) > 0 {
+		err = tagRepo.UntagPost(ctx, id, tagNames)
+		if err != nil {
+			if errors.Is(err, custom_errors.ErrTagNotFound) {
+				s.log.Debug("Tags not found for post during untag", slog.Int64("id", id))
+			} else {
+				s.log.Error("Failed to untag post", slog.String("error", err.Error()), slog.Int64("id", id))
+				return custom_errors.ErrTagDeleteFailed
+			}
+		}
+	}
+	err = postRepo.Delete(ctx, id)
+	if err != nil {
+		if errors.Is(err, custom_errors.ErrPostNotFound) {
+			s.log.Debug("Post not found for delete", slog.Int64("id", id))
+			return custom_errors.ErrPostNotFound
+		}
+		s.log.Error("Failed to delete post", slog.String("error", err.Error()), slog.Int64("id", id))
+		return custom_errors.ErrDatabaseQuery
+	}
+	err = tx.Commit(ctx)
+	if err != nil {
+		s.log.Error("Failed to commit transaction", slog.String("error", err.Error()))
+		return custom_errors.ErrDatabaseQuery
+	}
+	return nil
 }
