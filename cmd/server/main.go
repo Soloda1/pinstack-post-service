29df9279bb00462e1acf70a615d56fcf1c7848ee
Soloda1 <enderderder@gmail.com>
2025-06-29 00:20:59 +0400

    Feat: Добавил Prometheus

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
@@ -69,7 +72,18 @@ func main() {
 	postGRPCApi := post_grpc.NewPostGRPCService(postService, log)
 	grpcServer := delivery_grpc.NewServer(postGRPCApi, cfg.GRPCServer.Address, cfg.GRPCServer.Port, log)
 
-	done := make(chan bool)
+	metricsAddr := fmt.Sprintf("%s:%d", cfg.Prometheus.Address, cfg.Prometheus.Port)
+	metricsServer := &http.Server{
+		Addr:    metricsAddr,
+		Handler: nil,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+
+	done := make(chan bool, 1)
+	metricsDone := make(chan bool, 1)
+
 	go func() {
 		if err := grpcServer.Run(); err != nil {
 			log.Error("gRPC server error", slog.String("error", err.Error()))
@@ -77,15 +91,31 @@ func main() {
 		done <- true
 	}()
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		log.Info("Starting Prometheus metrics server", slog.String("address", metricsAddr))
+		if err := metricsServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Error("Prometheus metrics server error", slog.String("error", err.Error()))
+		}
+		metricsDone <- true
+	}()
+
 	<-quit
-	log.Info("Shutting down gRPC server...")
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
+	log.Info("Shutting down servers...")
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer shutdownCancel()
+
 	if err := grpcServer.Shutdown(); err != nil {
 		log.Error("gRPC server shutdown error", slog.String("error", err.Error()))
 	}
+
+	if err := metricsServer.Shutdown(shutdownCtx); err != nil {
+		log.Error("Metrics server shutdown error", slog.String("error", err.Error()))
+	}
+
 	<-done
-	log.Info("Server exiting")
+	<-metricsDone
+
+	log.Info("Server exited")
 }
